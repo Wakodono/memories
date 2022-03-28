66751b423837f6ee1ffb76e82102fb7efa9eb53a
Wakodono <53127265+Wakodono@users.noreply.github.com>
2022-03-28 13:23:38 +0100

    MongoDB

--- server/index.js
+++ server/index.js
@@ -3,8 +3,17 @@ import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 
+import 'dotenv/config'
+
 const app = express()
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
+
+const CONNECTION_URL = process.env.CONNECTION_URL
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+   .then(() => {app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`))})
+   .catch((err) => console.log(`${err} did not connect to the database`))
