f077d34ced29d81d8cabe58bfbae19efc0831b24
Wakodono <53127265+Wakodono@users.noreply.github.com>
2022-03-29 10:39:22 +0100

    Adds postRouter and controllers

--- server/index.js
+++ server/index.js
@@ -4,8 +4,11 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 
 import 'dotenv/config'
+import postRoutes from './routes/posts.js'
 
 const app = express()
+
+app.use('/posts', postRoutes)
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
@@ -16,4 +19,4 @@ const PORT = process.env.PORT || 5000
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
    .then(() => {app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`))})
-   .catch((err) => console.log(`${err} did not connect to the database`))
+   .catch((err) => console.log(`${err}`))
