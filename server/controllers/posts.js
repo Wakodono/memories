fa9b586a419a043325527d8976c563be6b5a97e8
Wakodono <53127265+Wakodono@users.noreply.github.com>
2022-03-30 14:44:04 +0100

    Updtates postMessage controller
    Adds get request logic for getPosts endpoint and adds express-list-endpoints

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,20 @@
-export const getPosts = (req, res) => {
-    res.send('Posts')
+import express from 'express'
+import mongoose from 'mongoose'
+
+import PostMessage from '../models/postMessage.js'
+
+const router = express.Router()
+
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find()
+
+        res.status(200).json(postMessages)
+    } catch (error) {
+        res.status(404).json({ message: error })
+    }
 }
 
 export const createPost = (req, res) => {}
+
+export default router
--- server/index.js
+++ server/index.js
@@ -5,18 +5,23 @@ import cors from 'cors'
 
 import 'dotenv/config'
 import postRoutes from './routes/posts.js'
+import listEndpoints from 'express-list-endpoints'
 
 const app = express()
 
-app.use('/posts', postRoutes)
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 
+app.use('/posts', postRoutes)
+
 const CONNECTION_URL = process.env.CONNECTION_URL
 const PORT = process.env.PORT || 5000
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-   .then(() => {app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`))})
+   .then(() => {app.listen(PORT, () => {
+       console.log(`Server Running on Port ${PORT}`)
+        console.table(listEndpoints(app))
+    })})
    .catch((err) => console.log(`${err}`))
