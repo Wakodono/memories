8ba09bd4b43aee9f73e8bfdc6b05b7fbef10d417
Wakodono <53127265+Wakodono@users.noreply.github.com>
2022-03-31 15:54:04 +0100

    Update posts.js

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -15,6 +15,18 @@ export const getPosts = async (req, res) => {
     }
 }
 
-export const createPost = (req, res) => {}
+export const createPost = async (req, res) => {
+    const post = req.body
+
+    const newPost = new PostMessage(post)
+
+    try {
+        await newPost.save()
+
+        res.status(201).json(newPost)
+    } catch (error) {
+        res.status(400).json({ message: error })
+    }
+}
 
 export default router
