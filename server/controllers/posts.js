e068ce9f9abf21b076cd27ae62487de51275f2cf
Wakodono <53127265+Wakodono@users.noreply.github.com>
2022-04-12 12:05:34 +0100

    Fixes mongose types check in updatePost controller

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -33,7 +33,7 @@ export const updatePost = async (req, res) => {
     const { id: _id } = req.params
     const post = req.body
 
-    if(mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('There is no post with tthat id')
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send(`There is no post with id: ${_id}`)
 
     const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true })
 
